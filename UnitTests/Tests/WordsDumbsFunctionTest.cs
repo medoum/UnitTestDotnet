e674c84a8023064e43c86afcce5d69bf257f5533
medoum <mohamedoum2430@gmail.com>
2024-01-16 14:56:53 +0100

    first commit

---
+++ UnitTests/Tests/WordsDumbsFunctionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace UnitTests.Tests
+{
+	public static class WordsDumbsFunctionTest
+	{
+		//Naming Convention - ClassName_Methode_ExcepctedResult
+		public static void WorldsDumbestFunction_ReturnPikachuIfzero_ReturnString()
+		{
+			try
+			{
+				//Arrange - Go get your variables, whatever you need , your classes, go get functions
+				int num = 0;
+                WordsDumbsFunction wordsDumbs = new WordsDumbsFunction();
+				//Act - Execute this funtion
+				string result = wordsDumbs.ReturnSPikachuIfZero(num);
+
+
+                //Assert - Whatever ever is returned is it what you want
+				if(result == "Pikachu")
+				{
+					Console.WriteLine("PASSED: WorldsDumbestFunction.ReturnPikachuIfzero_ReturnString");
+				}
+				else
+				{
+					Console.WriteLine("FAILED: WorldsDumbestFunction.ReturnPikachuIfzero_ReturnString");
+				}
+            }
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+	}
+}
+
---
+++ UnitTests/WordsDumbsFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace UnitTests
+{
+	public class WordsDumbsFunction
+	{
+		public WordsDumbsFunction()
+		{
+		}
+		public string ReturnSPikachuIfZero(int num)
+		{
+			if(num == 0)
+			{
+				return "Pikachu";
+			}
+			else
+			{
+				return "Squirtle";
+			}
+
+		}
+
+	}
+}
+
